11ec4d6e9d545d38ae8095c158ef6e748f4196a7
AyalMagid <ayal.magid@gmail.com>
2021-01-10 20:10:50 +0200

    jh

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<template>
+	<header class="grid-container">
+		<div class="site-header-wrapper grid-pos-center flex align-center space-between">
+			<section class="logo">
+				<h2>Logo</h2>
+			</section>
+			<nav>
+				<ul class="flex-center nav-links" :class="{ open: menuIsOpen }">
+					<router-link tag="li" to="about">Link1 </router-link>
+					<router-link tag="li" to="contact">Link </router-link>
+					<router-link tag="li" to="special">Link </router-link>
+				</ul>
+			</nav>
+			<section class="hamburger" @click="openMenu" />
+		</div>
+	</header>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			menuIsOpen: false,
+		};
+	},
+	methods: {
+		openMenu() {
+			this.menuIsOpen = !this.menuIsOpen;
+		},
+	},
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home pos-main-container">
+		<h1>Home</h1>
+		<BaseButton>Select</BaseButton>
+		<BaseInput label="my INPUT" @input="log" text="enter somthing" />
+		<BaseSelect label="Pick One.." :options="options" @select="log" />
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'Home',
+	data() {
+		return {
+			options: ['baba', 'gaga', 'dada'],
+		};
+	},
+	methods: {
+		log(data) {
+			console.log('clicked');
+		},
+	},
+	components: {},
+};
 </script>
